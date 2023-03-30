1286992b489c20eb0816cb9858cbe2ae8f27567d
Chuudim <chulanovdima2507@gmail.com>
2023-03-30 11:29:30 +0300

    6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Notiflix from "notiflix";
-import './css/styles.css';
+// import './css/styles.css';
 import debounce from 'lodash.debounce';
+src / css / styles.css
+import 'src/css/styles.css';
 
 
 const inputRef = document.querySelector("#search-box");
